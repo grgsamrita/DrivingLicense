0b295656a78f3c129fbac6bf6211ed3bd94c2b4f
grgsamrita <samrita.grg20@gmail.com>
2015-12-02 13:10:45 +0545

    migration created with model

---
+++ database/migrations/2015_12_02_071620_create_vouchers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('voucher_date');
+            $table->integer('voucher_id');
+            //image
+            $table->string('verification');
+            $table->date('ver_date');
+            $table->integer('ver_UID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('vouchers');
+    }
+}
---
+++ database/migrations/2015_12_02_071903_create_renews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRenewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('renews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('lic_id');
+            $table->date('renew_date');
+            $table->date('new_expiry');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('renews');
+    }
+}
